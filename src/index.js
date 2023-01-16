ae88c1e73ff28f98a7383f775d4baf50d05712ad
MarynaPoliarush <106143753+MarynaPoliarush@users.noreply.github.com>
2023-01-16 09:08:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,55 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+     let arr=[]
+
+
+    
+
+    for (let i=0; i<expr.replace(/\s/g, "").length;i+=10){ // replace(/\s/g, "") helps to get string length w/o spaces
+       if (expr.slice(i,i+10)[0]=='*'){
+        arr.push(['*'])
+        arr.push([...expr.slice(i+1,i+11)])
+       } else {
+        arr.push([...expr.slice(i,i+10)])
+       }
+    }
+
+   
+
+    let finArr=[]
+    for (let i=0; i<arr.length;i++){
+        finArr.push(new Array())
+        for (let j=0;j<9;j+=2){
+            
+            if ((arr[i][j]+arr[i][j+1])=='11'){
+                finArr[i] += ('-')
+            } else if ((arr[i][j]+arr[i][j+1])=='10'){
+                finArr[i] += ('.')
+            } else if(arr[i][j]=='*'){
+                finArr[i] +=('**********')
+            }
+        }
+    }
+
+    console.log(finArr)
+    let result=''
+    
+    for ( const item of finArr){
+        for ( key in MORSE_TABLE){
+            
+            if (item==key){
+                result+= MORSE_TABLE[key]
+            } 
+        }
+        if(item=='**********'){
+            result= result+ ' ';
+        }
+       
+    }
+
+  
+   return result
 }
 
 module.exports = {
